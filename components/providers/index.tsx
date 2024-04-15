1e15f2fe006f07a876e85d03254d35df87772427
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-04-15 17:04:49 +0200

    init

---
+++ components/providers/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
+
+import { SidebarProvider } from "@/lib/hooks/use-sidebar";
+
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+export default function Providers({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider {...props}>
+      <SidebarProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+      </SidebarProvider>
+    </NextThemesProvider>
+  );
+}
---
+++ lib/hooks/use-sidebar.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import * as React from "react"
+import { createContext, useContextSelector } from "use-context-selector"
+
+const LOCAL_STORAGE_KEY = "sidebar"
+
+type SidebarContext = {
+  isSidebarOpen: boolean
+  toggleSidebar: () => void
+  isLoading: boolean
+}
+
+const SidebarContext = createContext<SidebarContext | undefined>(undefined)
+
+export function useSidebar() {
+  const context = useContextSelector(SidebarContext, value => value)
+  if (!context) {
+    throw new Error("useSidebarContext must be used within a SidebarProvider")
+  }
+  return context
+}
+
+type SidebarProviderProps = {
+  children: React.ReactNode
+}
+
+export function SidebarProvider({ children }: SidebarProviderProps) {
+  const [isSidebarOpen, setSidebarOpen] = React.useState(true)
+  const [isLoading, setLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    const value = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (value) {
+      setSidebarOpen(JSON.parse(value))
+    }
+    setLoading(false)
+  }, [])
+
+  const toggleSidebar = () => {
+    setSidebarOpen(value => {
+      const newState = !value
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newState))
+      return newState
+    })
+  }
+
+  if (isLoading) {
+    return null
+  }
+
+  return (
+    <SidebarContext.Provider
+      value={{ isSidebarOpen, toggleSidebar, isLoading }}
+    >
+      {children}
+    </SidebarContext.Provider>
+  )
+}
