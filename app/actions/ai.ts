05508b2618a8af5bf689618b51bc300792679f81
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-05-30 19:51:31 +0200

    updates

--- app/actions/ai.ts
+++ app/actions/ai.ts
@@ -5,9 +5,75 @@ import { z } from "zod"
 
 import { formSchema } from "@/components/template/form-main"
 
-export async function callingAi(values: z.infer<typeof formSchema>) {
+type AiResponse = {
+  predictions: {
+    parallelfertigung: {
+      predictions: boolean[]
+      probabilities: number[][]
+    }
+    programmnr: {
+      predictions: number[]
+      probabilities: number[][]
+    }
+  }
+}
+
+export async function callingAi(
+  values: z.infer<typeof formSchema>
+): Promise<AiResponse | { error: string }> {
+  const dataBody = {
+    dataframe_split: {
+      columns: [
+        "FAHRZEUG_ID",
+        "KOPIERT_VON_ID",
+        "KATEGORIE",
+        "MODELL",
+        "KAROSSERIETYP",
+        "GEWICHT",
+        "HOEHE",
+        "KRAFTSTOFFTYP",
+        "KAROSSERIEFORM",
+        "LEISTUNG_KATEGORIE",
+        "GETRIEBE",
+        "LACKFARBE",
+        "INFOTAINMENT",
+        "KOMFORT",
+        "SICHERHEITSSYSTEME"
+      ],
+      data: [
+        [
+          values.fahrzeug_id,
+          null,
+          values.kategorie,
+          values.modell,
+          values.karosserietyp,
+          Number(values.gewicht),
+          Number(values.hoehe),
+          values.kraftstofftyp,
+          values.karosserieform,
+          values.leistung_kategorie,
+          values.getriebe,
+          values.lackfarbe,
+          values.infotainment,
+          values.komfort,
+          values.sicherheitssystem
+        ]
+      ]
+    }
+  }
+
   try {
-    // TODO: Implement the API call to the AI model
+    const { data } = await axios.post(
+      "http://127.0.0.1:5001/invocations",
+      dataBody,
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    )
+
+    return data
   } catch (error) {
     return {
       error: "Something went wrong"
