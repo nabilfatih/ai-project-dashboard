739e44eeab1d07cf8255a28a7800c5ff7944165b
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-05-30 13:58:41 +0200

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <section className="py-12">
       <div className="relative mx-auto max-w-7xl px-4">
-        <div className="pb-10">
+        <div className="pb-8">
           <h1 className="font-serif text-2xl font-medium tracking-tighter">
             Vehicle Assembly
           </h1>
